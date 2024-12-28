ea8666f551aaf485c3ad90e9330e116ede83fa05
blackblackout483 <blackblackout483@gmail.com>
2024-12-28 16:58:14 +0300

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+is_prime = True
+primes = []
+not_primes = []
+
+for i in range(2, len(numbers)):
+    for j in range(2, i):
+        if i % j == 0:
+            is_prime = False
+            not_primes.append(i)
+            break
+    else:
+        primes.append(i)
+
+print('Primes:', primes)
+print('Not_primes:', not_primes)
